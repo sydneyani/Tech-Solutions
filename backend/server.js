2de96fea5dfe1e0728f7273440e8e21c009064b9
sydneyani <135778566+sydneyani@users.noreply.github.com>
2025-04-19 14:41:42 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: '*',
+  origin: 'https://tech-solutions-production-e796.up.railway.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
